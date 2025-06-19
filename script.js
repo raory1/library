03d05867715c4817d88552d01e780220055e675d
Isabella <isa021andrade@gmail.com>
2025-06-19 20:37:23 -0300

    feat: add createBook function

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 console.log("ok")
 
-function Book(){
+function Book(title, author, pages){
     if(!new.target){
         throw Error("You must use the 'new' operator to call the constructor");
     }
     this.title = title
     this.author = author
     this.pages = pages
-    this.isFinished = isFinished
     this.info = function(){
-        return(`${this.title}, ${this.author}, ${this.pages} pages, ${isFinished}`)
+        return(`${this.title}, ${this.author}, ${this.pages} pages`)
      }
 }
+
+const books = []
+function createBook(title, author, pages){
+    const book = new Book(title, author, pages)
+    books.push(book)
+}
+
+createBook("The Hobbit", "", "230")
+console.log(books)
