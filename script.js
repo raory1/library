e06a38877854b38a06844d92974eab7de0fb3d6a
Isabella <isa021andrade@gmail.com>
2025-06-19 20:29:49 -0300

    feat: create book constructor

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("ok")
+
+function Book(){
+    if(!new.target){
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.isFinished = isFinished
+    this.info = function(){
+        return(`${this.title}, ${this.author}, ${this.pages} pages, ${isFinished}`)
+     }
+}
