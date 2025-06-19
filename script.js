289aada0b8c65f177827501fee0bd858610d1bea
Isabella <isa021andrade@gmail.com>
2025-06-19 20:40:18 -0300

    feat: add unique id to book constructor

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function Book(title, author, pages){
     if(!new.target){
         throw Error("You must use the 'new' operator to call the constructor");
     }
+    this.id = crypto.randomUUID()
     this.title = title
     this.author = author
     this.pages = pages
