b0d00de413465c24a73a789c6a10e37a118cceb6
Isabella <isa021andrade@gmail.com>
2025-06-28 14:05:38 -0300

    adjust indentation

--- script.js
+++ script.js
@@ -1,96 +1,94 @@
-const library = []
+const library = [];
 const showBtn = document.getElementById("show-dialog");
 const dialog = document.getElementById("dialog");
 const submitBtn = dialog.querySelector("#submitBtn");
 const cancelBtn = dialog.querySelector("#cancelBtn");
-const bookTitle = document.getElementById("title")
-const bookAuthor = document.getElementById("author")
-const bookPages = document.getElementById("pages")
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
 const form = document.getElementById("book-form");
 
-const table = document.getElementById("books-table")
-const tbody = document.querySelector("#books-table tbody")
+const table = document.getElementById("books-table");
+const tbody = document.querySelector("#books-table tbody");
 
-function Book(title, author, pages, status){
-    if(!new.target){
+function Book(title, author, pages, status) {
+    if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.id = crypto.randomUUID()
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
-function removeBookFromLibrary(id){
+function removeBookFromLibrary(id) {
     const index = library.findIndex((book) => {
-        return book.id === id
-    })
-    library.splice(index, 1)
-    displayBooks()
+        return book.id === id;
+    });
+    library.splice(index, 1);
+    displayBooks();
 }
 
-function addBookToLibrary(title, author, pages, status){
-    const book = new Book(title, author, pages, status)
-    library.push(book)
+function addBookToLibrary(title, author, pages, status) {
+    const book = new Book(title, author, pages, status);
+    library.push(book);
 }
 
 function toggleReadStatus(id) {
     library.find((book) => {
-        if(book.id === id){
-            book.status = !book.status
+        if (book.id === id) {
+            book.status = !book.status;
         }
-    })
-    displayBooks()
+    });
+    displayBooks();
 }
 
-function displayBooks(){
-    tbody.innerHTML = ""
-    library.forEach(book => {
-        const tr = document.createElement("tr")
-        for(const property in book) {
-            const td = document.createElement("td")
-            if (property === "status"){
-                const checkbox = document.createElement("input")
-                checkbox.type = "checkbox"
+function displayBooks() {
+    tbody.innerHTML = "";
+    library.forEach((book) => {
+        const tr = document.createElement("tr");
+        for (const property in book) {
+            const td = document.createElement("td");
+            if (property === "status") {
+                const checkbox = document.createElement("input");
+                checkbox.type = "checkbox";
                 checkbox.checked = book.status;
-                checkbox.addEventListener("change", () =>{
-                    toggleReadStatus(book.id)
-                })
-                td.append(checkbox)
+                checkbox.addEventListener("change", () => {
+                    toggleReadStatus(book.id);
+                });
+                td.append(checkbox);
+            } else {
+                td.innerText = book[property];
             }
-            else {
-                td.innerText = book[property]
-            }
-            tr.append(td)
+            tr.append(td);
         }
-        tr.dataset.index = book.id
-        tbody.append(tr)
-    })
+        tr.dataset.index = book.id;
+        tbody.append(tr);
+    });
 }
 
-addBookToLibrary("The Hobbit", "aa", "230", true)
-addBookToLibrary("O guarani", "José de Alencar", "230", true)
-addBookToLibrary("A Sereia", "Kiera Cass", "200", false)
+addBookToLibrary("The Hobbit", "aa", "230", true);
+addBookToLibrary("O guarani", "José de Alencar", "230", true);
+addBookToLibrary("A Sereia", "Kiera Cass", "200", false);
 
 showBtn.addEventListener("click", () => {
-  dialog.showModal();
+    dialog.showModal();
 });
 
 submitBtn.addEventListener("click", (e) => {
-    if(form.checkValidity() === true){
+    if (form.checkValidity() === true) {
         e.preventDefault();
-        addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value)
-        displayBooks()
+        addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value);
+        displayBooks();
         form.reset();
         dialog.close();
     }
 });
 
-cancelBtn.addEventListener("click", (e) =>{
+cancelBtn.addEventListener("click", (e) => {
     e.preventDefault();
     dialog.close();
-})
+});
 
-
-displayBooks()
+displayBooks();
